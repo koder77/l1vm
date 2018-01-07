313ae1d5f6ebf876821b9f468956cb77d984ec7c
koder77 <jay-t@gmx.net>
2018-01-07 15:31:47 +0100

    RS232 library main.c

---
+++ vm/modules/rs232/main.c
@@ -0,0 +1,231 @@
+/*
+ * This file rs232.c is part of L1vm.
+ *
+ * (c) Copyright Stefan Pietzonke (jay-t@gmx.net), 2017
+ *
+ * L1vm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * L1vm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../../../include/global.h"
+#include "../../../include/stack.h"
+
+#include "rs232.h"
+
+U1 *rs232_OpenComport (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    S8 portnumber ALIGN;
+    S8 baudrate ALIGN;
+    U1 mode[256];
+    S8 ret ALIGN;
+    S8 i ALIGN = 0;
+
+    while (*sp != 0)
+    {
+       if (i < 256)
+       {
+           mode[i] = *sp;
+           i++;
+           sp++;
+           if (sp == sp_top)
+           {
+               printf ("FATAL ERROR: file_open: stack corrupt!\n");
+               return (NULL);
+           }
+       }
+    }
+    mode[i] = '\0';        // end of string
+
+    sp = stpopi ((U1 *) &baudrate, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_OpenComport: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    sp = stpopi ((U1 *) &portnumber, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_OpenComport: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    ret = RS232_OpenComport (portnumber, baudrate, (const char *) mode);
+
+    sp = stpushi (ret, sp, sp_bottom);
+    if (sp == NULL)
+    {
+      // error
+      printf ("rs232_OpenComport: ERROR: stack corrupt!\n");
+      return (NULL);
+    }
+
+    return (sp);
+}
+
+U1 *rs232_PollComport (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    S8 portnumber ALIGN;
+    S8 size ALIGN;
+    S8 i ALIGN;
+	S8 ret ALIGN;
+
+	U1 buf[4096];
+
+    sp = stpopi ((U1 *) &size, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_PollComport: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    sp = stpopi ((U1 *) &portnumber, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_PollComport: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    ret = RS232_PollComport (portnumber, buf, size);
+
+    for (i = 0; i < ret; i++)
+	{
+        sp = stpushb (buf[i], sp, sp_bottom);
+    	if (sp == NULL)
+    	{
+    		// error
+    		printf ("rs232_PollComport: ERROR: stack corrupt!\n");
+    		return (NULL);
+    	}
+    }
+
+    // push number of received bytes on stack
+    sp = stpushi (ret, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("rs232_PollComport: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    return (sp);
+}
+
+U1 *rs232_SendByte (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    S8 portnumber ALIGN;
+	S8 ret ALIGN;
+	U1 byte;
+
+    sp = stpopb (&byte, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_SendByte: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    sp = stpopi ((U1 *) &portnumber, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_SendByte: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    ret = RS232_SendByte (portnumber, byte);
+
+    sp = stpushi (ret, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("rs232_PollComport: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    return (sp);
+}
+
+U1 *rs232_SendBuf (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    S8 portnumber ALIGN;
+    S8 size ALIGN;
+    S8 i ALIGN;
+	S8 ret ALIGN;
+	U1 byte;
+
+	U1 buf[4096];
+
+    sp = stpopi ((U1 *) &size, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_SendBuf: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    // get send buffer from stack
+    for (i = 0; i < size; i++)
+    {
+        sp = stpopb (&byte, sp, sp_top);
+    	if (sp == NULL)
+    	{
+    		// error
+    		printf ("rs232_SendBuf: ERROR: stack corrupt!\n");
+    		return (NULL);
+    	}
+
+        buf[i] = byte;
+    }
+
+    sp = stpopi ((U1 *) &portnumber, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_SendBuf: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    ret = RS232_SendBuf (portnumber, buf, size);
+
+    sp = stpushi (ret, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("rs232_SendBuf: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    return (sp);
+}
+
+U1 *rs232_CloseComport (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    S8 portnumber ALIGN;
+
+    sp = stpopi ((U1 *) &portnumber, sp, sp_top);
+	if (sp == NULL)
+	{
+		// error
+		printf ("rs232_CloseComport: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+
+    RS232_CloseComport (portnumber);
+
+    return (sp);
+}
