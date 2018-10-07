5101ef86c6ecdbf520121dd2713db493ca5e039b
koder77 <jay-t@gmx.net>
2018-10-07 17:25:46 +0200

    if.h copyright
    if.h copyright

--- comp/if.h
+++ comp/if.h
@@ -1,3 +1,22 @@
+/*
+ * This file if.h is part of L1vm.
+ *
+ * (c) Copyright Stefan Pietzonke (jay-t@gmx.net), 2018
+ *
+ * L1vm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * L1vm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define MAXJUMPLIST 1024
 #define MAXJUMPNAME 64
 #define MAXIF 1024
