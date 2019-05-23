d12bae26c4823e2cdf80497fa4c97a8792c9c164
koder77 <jay-t@gmx.net>
2019-05-23 22:23:58 +0200

    string.c added

---
+++ string/string.c
@@ -0,0 +1,41 @@
+/*
+ * This file string.c is part of L1vm.
+ *
+ * (c) Copyright Stefan Pietzonke (jay-t@gmx.net), 2017
+ *
+ * L1vm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * L1vm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../include/global.h"
+
+ size_t strlen_safe (const char * str, int maxlen)
+ {
+	 long long int i = 0;
+
+	 while (1)
+	 {
+	 	if (str[i] != '\0')
+		{
+			i++;
+		}
+		else
+		{
+			return (i);
+		}
+		if (i > maxlen)
+		{
+			return (0);
+		}
+	}
+}
