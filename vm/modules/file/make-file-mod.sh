bc8c9b3102cdc2e6da4d19502de21ebb4f29d4fd
koder77 <jay-t@gmx.net>
2019-05-30 19:56:15 +0200

    Changed makefiles for new pathname of string.c.

--- vm/modules/file/make-file-mod.sh
+++ vm/modules/file/make-file-mod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-clang -Wall -fPIC -g -c file.c ../../../string/string.c -O3 -fomit-frame-pointer -g
+clang -Wall -fPIC -g -c file.c ../../../lib-func/string.c -O3 -fomit-frame-pointer -g
 clang -shared -Wl,-soname,libl1vmfile.so.1 -o libl1vmfile.so.1.0 file.o
 cp libl1vmfile.so.1.0 libl1vmfile.so
