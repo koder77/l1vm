69ff84da809832da3505f3b0cbf79e401503fdca
koder77 <jay-t@gmx.net>
2019-06-21 15:00:39 +0200

    vm/make-nojit.sh update

--- vm/make-nojit.sh
+++ vm/make-nojit.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-clang main.c load-object.c ../lib-func/string.c -o l1vm-nojit -ldl -lpthread -lstdc++ -lSDL -lSDL_gfx -lSDL_image -lSDL_ttf -Os -fomit-frame-pointer -g -Wl,--export-dynamic
-#! without SDL library support:
-#! clang main.c load-object.c ../string/string.c -o l1vm -ldl -lpthread -lasmjit -lstdc++ -Os -fomit-frame-pointer -g -Wl,--export-dynamic
+# set vm/main.c JIT_COMPILER to 0 and compile using this script
+clang -Wall main.c load-object.c ../lib-func/string.c -o l1vm-nojit -ldl -lpthread -lstdc++ -lSDL -lSDL_gfx -lSDL_image -lSDL_ttf -Os -fomit-frame-pointer -g -Wl,--export-dynamic
+# without SDL library support:
+# clang -Wall main.c load-object.c ../string/string.c -o l1vm -ldl -lpthread -lasmjit -lstdc++ -Os -fomit-frame-pointer -g -Wl,--export-dynamic
