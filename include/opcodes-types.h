808bb5516f1dfbbb35e1023b974d4bcbfc751e22
koder77 <jay-t@gmx.net>
2020-01-30 18:03:19 +0100

    compiler source code split

---
+++ include/opcodes-types.h
@@ -0,0 +1,28 @@
+/*
+ * This file opcodes-types.h is part of L1vm.
+ *
+ * (c) Copyright Stefan Pietzonke (jay-t@gmx.net), 2017
+ *
+ * L1vm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * L1vm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// l1vm - opcodes
+
+#define EMPTY       0
+#define I_REG       1
+#define D_REG       2
+#define DATA        3
+#define DATA_OFFS   4
+#define LABEL       5
+#define ALL         6
