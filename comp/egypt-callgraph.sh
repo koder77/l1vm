079a47a89712cc4f5522dc7077f40d849c3a6a6c
koder77 <jay-t@gmx.net>
2020-01-30 18:46:33 +0100

    compiler egypt call graph script update

--- comp/egypt-callgraph.sh
+++ comp/egypt-callgraph.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm *.o
-gcc -Wall main.c ../lib-func/file.c checkd.c if.c mem.c ../lib-func/string.c -fdump-rtl-expand -o l1com -g 
+gcc -Wall main.c labels.c register.c var.c ../lib-func/file.c checkd.c if.c mem.c ../lib-func/string.c -fdump-rtl-expand -o l1com -g 
 
 egypt *.expand | dot -Grankdir=LR -Tps -o callgraph.ps
 rm *.o
