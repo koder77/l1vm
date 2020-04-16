58c6e0fd0b12c7d15fa5c20c06ddb69889ad6b5a
koder77 <jay-t@gmx.net>
2020-04-16 12:48:20 +0200

    program libraries build script

--- install.sh
+++ install.sh
@@ -101,16 +101,7 @@ else
 	exit 1
 fi
 
-echo "all modules build. building program libraries..."
-cd ../lib
-if ./build-all.sh; then
-	echo "building libraries successfully!"
-else
-	echo "building libraries FAILED!"
-	exit 1
-fi
-
-echo "all libraries installed. building programs..."
+echo "all modules installed. building programs..."
 cd ../prog
 if ./build-all.sh; then
 	echo "building programs successfully!"
@@ -119,8 +110,6 @@ else
 	exit 1
 fi
 cd ..
-
-
 
 echo "installation finished!"
 exit 0
