f2649f93d1b3cea6ef67f4316e3b6662e020ca32
koder77 <jay-t@gmx.net>
2020-05-28 14:23:36 +0200

    update

--- modules/build-df.sh
+++ modules/build-df.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 set -e
 cd ../vm/modules/endianess
+chmod +x *.sh
 ./make-endianess-mod.sh
 cd ../fann
+chmod +x *.sh
 ./make-fann-mod.sh
 cd ../file
+chmod +x *.sh
 ./make-file-mod.sh
 cd ../genann
+chmod +x *.sh
 ./make-genann-mod.sh
 cd ../math
+chmod +x *.sh
 ./make-math-mod.sh
 cd ../mpfr-c++
+chmod +x *.sh
 ./make-mpfr.sh
 cd ../net
+chmod +x *.sh
 ./make-net-mod.sh
 cd ../process
+chmod +x *.sh
 ./make-process-mod.sh
 cd ../rs232
+chmod +x *.sh
 ./make-rs232-mod.sh
-cd ../sdl
+cd ../sdl-2.0
+chmod +x *.sh
 ./make-sdl-mod.sh
 cd ../string
+chmod +x *.sh
 ./make-string-mod.sh
 cd ../time
+chmod +x *.sh
 ./make-time-mod.sh
 cd ../mem
+chmod +x *.sh
 ./make-mem-mod.sh
 
 cd ../../../modules
@@ -37,9 +50,8 @@ cp ../vm/modules/mpfr-c++/libl1vm* .
 cp ../vm/modules/net/libl1vm* .
 cp ../vm/modules/process/libl1vm* .
 cp ../vm/modules/rs232/libl1vm* .
-cp ../vm/modules/sdl/libl1vm* .
+cp ../vm/modules/sdl-2.0/libl1vm* .
 cp ../vm/modules/string/libl1vm* .
 cp ../vm/modules/time/libl1vm* .
 cp ../vm/modules/mem/libl1vm* .
-
 # sudo cp libl1vm* /usr/local/lib
