02d2e0c9aec21c7e6dea9be78a3883303de1e8af
Stefan Pietzonke <spietzonke@gmail.com>
2020-06-13 21:06:48 +0200

    makefile cleanup

--- vm/make.sh
+++ vm/make.sh
@@ -2,6 +2,6 @@
 # $CCPP -Wall jit.cpp -c -I/usr/local/include -O3 -fomit-frame-pointer
 #clang -Wall main.c load-object.c jit.o ../lib-func/string.c -o l1vm -lm -ldl -lpthread -lasmjit -lstdc++ -lSDL -lSDL_gfx -lSDL_image -lSDL_ttf -Os -fomit-frame-pointer -g -Wl,--export-dynamic
 
-$CC -Wall main.c load-object.c ../lib-func/string.c ../lib-func/code_datasize.c ../lib-func/file.c -o l1vm -L/usr/local/lib -lstdc++ -lm -ldl -lpthread -lasmjit -ll1vm-jit -lSDL2 -lSDL2_gfx -lSDL2_image -lSDL2_ttf -Os -fomit-frame-pointer -g -Wl,--export-dynamic
+$CC -Wall main.c load-object.c ../lib-func/string.c ../lib-func/code_datasize.c -o l1vm -L/usr/local/lib -lstdc++ -lm -ldl -lpthread -lasmjit -ll1vm-jit -lSDL2 -lSDL2_gfx -lSDL2_image -lSDL2_ttf -O2 -fomit-frame-pointer -g -Wl,--export-dynamic
 #! without SDL library support:
 #! clang main.c load-object.c jit.o ../string/string.c -o l1vm -ldl -lpthread -lasmjit -lstdc++ -Os -fomit-frame-pointer -g -Wl,--export-dynamic
