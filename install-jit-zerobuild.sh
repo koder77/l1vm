caef72ac89a7f46d7ff7d173fa40552216a2aeb0
Stefan Pietzonke <spietzonke@gmail.com>
2020-06-14 09:26:13 +0200

    1.0.5 - zerobuild used for building the programs

---
+++ install-jit-zerobuild.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# changed: install to /home/foo/bin instead to /usr/local/bin!
+
+echo "building compiler, assembler and VM..."
+
+export CC=clang
+export CCPP=clang++
+
+cd assemb
+if zerobuild force; then
+	echo "l1asm build ok!"
+else
+	echo "l1asm build error!"
+	exit 1
+fi
+
+cd ../comp
+if zerobuild force; then
+	echo "l1com build ok!"
+else
+	echo "l1com build error!"
+	exit 1
+fi
+
+cd ../vm
+if zerobuild force; then
+	echo "l1vm JIT build ok!"
+else
+	echo "l1vm JIT build error!"
+	exit 1
+fi
+cp l1vm l1vm-jit
+cd ..
+cp assemb/l1asm ~/bin
+cp comp/l1com ~/bin
+cp vm/l1vm-jit ~/bin
+echo "VM binaries installed into ~/bin"
+
+cd modules
+echo "installing modules..."
+chmod +x *.sh
+./build.sh
+if ./install.sh; then
+	echo "modules build ok!"
+else
+	echo "modules build FAILED!"
+	exit 1
+fi
+
+echo "all modules installed. building programs..."
+cd ../prog
+chmod +x *.sh
+if ./build-all.sh; then
+	echo "building programs successfully!"
+else
+	echo "building programs FAILED!"
+	exit 1
+fi
+cd ..
+
+echo "installation finished!"
+exit 0
