5af535195b4c94c16a4e744de7592b4045c955ef
Stefan Pietzonke <spietzonke@gmail.com>
2020-09-13 19:34:39 +0200

    build scripts now clone libasmjit and build it

--- install-jit-zerobuild-fedora.sh
+++ install-jit-zerobuild-fedora.sh
@@ -46,6 +46,24 @@ else
 	cd ..
 fi
 
+# check if libasmjit is installed
+FILE=/usr/local/lib/libasmjit.so
+if test -f "$FILE"; then
+    echo "$FILE exists!"
+else
+	echo "libasmjit not installed into $FILE!"
+	echo "cloning and building it now..."
+	git clone https://github.com/asmjit/asmjit
+	cd asmjit
+	mkdir build
+	cd build
+	cmake ../
+	make
+	sudo make install
+	cd ..
+	cd ..
+fi
+
 cd assemb
 if zerobuild force; then
 	echo "l1asm build ok!"
