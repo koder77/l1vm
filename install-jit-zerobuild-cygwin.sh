96b5a2c3a647287948bb92dbaf1d9f024799e450
Stefan Pietzonke <spietzonke@gmail.com>
2020-10-12 22:06:48 +0200

    Cygwin build script update

---
+++ install-jit-zerobuild-cygwin.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+# changed: install to /home/foo/bin instead to /usr/local/bin!
+
+echo "building compiler, assembler and VM..."
+
+export CC=clang
+export CCPP=clang++
+
+# check if ~/bin exists
+DIR="~/bin"
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "${DIR} already exists!"
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "${DIR} will be created now..."
+  mkdir ~/bin
+fi
+
+# check if zerobuild installed into ~/bin
+FILE=~/bin/zerobuild
+if test -f "$FILE"; then
+    echo "$FILE exists!"
+else
+	echo "zerobuild not installed into $FILE!"
+	echo "cloning and building it now..."
+	git clone https://github.com/koder77/zerobuild.git
+	cd zerobuild
+	./make.sh
+	cp zerobuild ~/bin/
+	cd ..
+fi
+
+# check if libasmjit is installed
+FILE=/usr/local/lib/libasmjit.dll
+if test -f "$FILE"; then
+    echo "$FILE exists!"
+else
+	echo "libasmjit not installed into $FILE!"
+	echo "cloning and building it now..."
+	git clone https://github.com/asmjit/asmjit
+	cd asmjit
+	mkdir build
+	cd build
+	cmake ../
+	make
+	make install
+	cp cygasmjit.dll /usr/local/lib/libasmjit.dll
+	cd ..
+	cd ..
+fi
+
+# check if libl1vm-jit.so is installed
+FILE=/usr/local/liblibl1vm-jit.dll
+if test -f "$FILE"; then
+    echo "$FILE exists!"
+else
+	echo "libl1vm-jit not installed into $FILE!"
+	echo "building it now..."
+	cd libjit
+	chmod +x make-win.sh
+	if ./make-win.sh; then
+		echo "libl1vm-jit build ok!"
+	else
+		echo "libl1vm-jit build error!"
+		exit 1
+	fi
+	cp libl1vm-jit.so /usr/local/lib/libl1vm-jit.dll
+	cd ..
+fi
+
+cd assemb
+chmod +x make-win.sh
+if ./make-win.sh; then
+	echo "l1asm build ok!"
+else
+	echo "l1asm build error!"
+	exit 1
+fi
+
+cd ../comp
+chmod +x make-win.sh
+if ./make-win.sh; then
+	echo "l1com build ok!"
+else
+	echo "l1com build error!"
+	exit 1
+fi
+
+cd ../vm
+chmod +x make-win-jit.sh
+if ./make-win-jit.sh; then
+	echo "l1vm JIT build ok!"
+else
+	echo "l1vm JIT build error!"
+	exit 1
+fi
+cp l1vm l1vm-nojit
+cd ..
+cp assemb/l1asm ~/bin
+cp comp/l1com ~/bin
+cp vm/l1vm ~/bin
+echo "VM binaries installed into ~/bin"
+
+cd modules
+echo "installing modules..."
+chmod +x *.sh
+./build-win.sh
+if ./install-win.sh; then
+	echo "modules build ok!"
+else
+	echo "modules build FAILED!"
+	exit 1
+fi
+
+echo "all modules installed. building programs..."
+cd ../prog
+chmod +x *.sh
+if ./build-all.sh; then
+	echo "building programs successfully!"
+else
+	echo "building programs FAILED!"
+	exit 1
+fi
+cd ..
+
+echo "installation finished!"
+exit 0
