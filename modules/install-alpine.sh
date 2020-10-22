30fad810f916085910584d725662a277bac7a762
Stefan Pietzonke <spietzonke@gmail.com>
2020-10-22 20:40:01 +0200

    build scripts update

---
+++ modules/install-alpine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# install modules to /usr/local/lib
+numberOfFiles=$(find . -type f | wc -l)
+if [ $numberOfFiles != 21 ]
+then
+	echo "ERROR building modules! Some modules failed to build!"
+	exit 1
+else
+	echo "Building modules was successfull! Installing..."
+	cp libl1vm* ~/bin
+	exit 0
+fi
