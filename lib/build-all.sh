204e976c4b970b5dfc18cd3dadbfce861230ff10
Stefan Pietzonke <spietzonke@gmail.com>
2020-12-25 01:12:20 +0100

    lib/ build script update

--- lib/build-all.sh
+++ lib/build-all.sh
@@ -3,10 +3,10 @@ for i in *.l1com
 do
 	l1pre $i out.l1com ~/l1vm/include/
 	l1com out
-	outname="${i}.l1obj"
-	cp out.l1obj $outname
 	RETVAL=$?
 	[ $RETVAL -ne 0 ] && echo "build failed: " && echo $i && exit 1
+	outname="${i}.l1obj"
+	cp out.l1obj $outname
 done
 for j in *.l1asm
 do
