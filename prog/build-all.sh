dc83634a677804119f2111bbccb09735a2deaf7a
Stefan Pietzonke <spietzonke@gmail.com>
2021-01-23 19:52:29 +0100

    new prog/build-all.sh script

--- prog/build-all.sh
+++ prog/build-all.sh
@@ -7,7 +7,10 @@ do
 done
 for i in *.l1com
 do
-	l1com $i -sizes 1000000 1000000000
+	l1pre $1 out.l1com ~/l1vm/include/
+	RETVAL=$?
+	[ $RETVAL -ne 0 ] && echo "preprocessor build failed: " && echo $i && exit 1
+	l1com out -sizes 1000000 1000000000
 	RETVAL=$?
 	[ $RETVAL -ne 0 ] && echo "build failed: " && echo $i && exit 1
 done
