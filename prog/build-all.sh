369ca04b58c5c6bb5aca2eb732332c3ff8123d05
Stefan Pietzonke <spietzonke@gmail.com>
2021-05-07 23:03:23 +0200

    prog/build-all.sh bugfixes

--- prog/build-all.sh
+++ prog/build-all.sh
@@ -7,7 +7,7 @@ do
 done
 for i in *.l1com
 do
-	l1pre $1 out.l1com ~/l1vm/include/
+	l1pre $i out.l1com ~/l1vm/include/
 	RETVAL=$?
 	[ $RETVAL -ne 0 ] && echo "preprocessor build failed: " && echo $i && exit 1
 	l1com out -sizes 1000000 1000000000
