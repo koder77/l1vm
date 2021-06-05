787133fe3145463bf33c7f6d513f251e95999b1a
Stefan Pietzonke <spietzonke@gmail.com>
2021-06-05 06:23:34 +0200

    compiler: math expressions bugfixes. See ChangeLog!

--- include/global.h
+++ include/global.h
@@ -27,8 +27,13 @@
 	#define __linux__	1
 #endif
 
+// new for parse-infix.c compiler
+#include <sys/types.h>
+#include <regex.h>
+// ------------------------------
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <stdarg.h>
 #include <signal.h>
 #include <stdint.h>
