5d383c643df06a8f7898390dec555f12b395f327
Stefan Pietzonke <spietzonke@gmail.com>
2021-07-24 16:56:42 +0200

    Updated osv example library

--- l1vm-osv-capstan-lib/build.sh
+++ l1vm-osv-capstan-lib/build.sh
@@ -8,9 +8,9 @@ echo "building l1vm-osv.so"
 zerobuild zerobuild-osv.txt force
 cp l1vm-osv.so ../l1vm-osv-capstan-lib
 cd ../lib
-cp math-lib.l1obj ../l1vm-osv-capstan-lib
+cp string.l1obj ../l1vm-osv-capstan-lib
 cd ../l1vm-osv-capstan-lib
-cp ../vm/modules/math/libl1vmmath.so .
+cp ../vm/modules/string/libl1vmstring.so .
 capstan package pull osv.bootstrap
 capstan package compose l1vm
 echo "running OSv image using qemu..."
