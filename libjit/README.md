c8d355c6ec211402214c61debd4cd1fb90181f2b
Stefan Pietzonke <spietzonke@gmail.com>
2021-09-13 17:15:40 +0200

    VM: JIT-compiler now OS too!

--- libjit/README.md
+++ libjit/README.md
@@ -1,17 +1,12 @@
-L1VM jit-library DEMO README
-============================
-In this directory is the L1VM JIT-compiler demo library.
-There are versions for Linux 64 bit (tested on Fedora 33) and Windows 10 WSL (Debian).
-You need libcrypto installed on your system.
-The demo version is limited to 40 opcodes which can be JIT-compiled!
-You can get the full version in my ko-fi shop: https://ko-fi.com/koder77/shop
-
+L1VM jit-library README
+=======================
+In this directory is the L1VM JIT-compiler library.
 Also the asmjit library is needed: https://github.com/asmjit/asmjit
 
 The library libl1vm-jit.so must be installed into /usr/local/lib or
 another library search path.
 
-In the L1VM JIT-compiler version are the following opodes included:
+In the L1VM JIT-compiler version are the following opcodes included:
 
 addi, subi, muli, divi <br>
 addd, subd, muld, divd <br>
---
+++ libjit/jit.h
@@ -0,0 +1,28 @@
+/*
+ * This file jit.h is part of L1vm.
+ *
+ * (c) Copyright Stefan Pietzonke (jay-t@gmx.net), 2020
+ *
+ * L1vm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * L1vm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//  l1vm RISC VM
+//
+//
+
+// set to 1 to build with JIT compiler
+#define JIT_COMPILER 1
+
+#define MAXJITCODE 40960
+#define MAXJUMPLEN 40960
