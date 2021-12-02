bf14ce73ef8d38346ca8eb6797c858fca0c9fce6
Stefan Pietzonke <spietzonke@gmail.com>
2021-12-02 23:42:28 +0100

    macOS build script

--- install-zerobuild-macos.sh
+++ install-zerobuild-macos.sh
@@ -60,7 +60,7 @@ fi
 echo "mpreal.h installation"
 echo "cloning and building it now..."
 git clone https://github.com/advanpix/mpreal.git
-cd mpreal
+# cd mpreal
 # sudo cp mpreal.h /usr/local/include
 cp vm/modules/mpfr-c++/mpreal.h /usr/local/include
 cd ..
