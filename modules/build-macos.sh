bb65fd857a247a3a04b093d5127196ec10bdde4f
Stefan Pietzonke <spietzonke@gmail.com>
2021-12-17 12:31:10 +0100

    macOS VM: mpfrc++ module build try

--- modules/build-macos.sh
+++ modules/build-macos.sh
@@ -22,12 +22,10 @@ cd ../string
 zerobuild zerobuild-macos.txt force
 cd ../time
 zerobuild zerobuild-macos.txt force
-# cd ../mpfr-c++
-# zerobuild zerobuild-macos.txt force
-# build of mem module fails!
 cd ../mem
 zerobuild zerobuild-macos.txt force
-
+cd ../mpfr-c++
+zerobuild zerobuild-macos.txt force
 
 cd ../../../modules
 cp ../vm/modules/endianess/libl1vm* .
@@ -42,6 +40,6 @@ cp ../vm/modules/sdl-2.0/libl1vm* .
 cp ../vm/modules/string/libl1vm* .
 cp ../vm/modules/time/libl1vm* .
 cp ../vm/modules/mem/libl1vm* .
-# cp ../vm/modules/mpfr-c++/libl1vm* .
+cp ../vm/modules/mpfr-c++/libl1vm* .
 
 # sudo cp libl1vm* /usr/local/lib
