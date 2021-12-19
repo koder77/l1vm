9b2998f748632fef95afde620af866922ff72b56
Stefan Pietzonke <spietzonke@gmail.com>
2021-12-19 18:07:36 +0100

    VM: build scripts update

--- vm/make-win.sh
+++ vm/make-win.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clang -Wall -Wextra main.c load-object.c ../lib-func/string.c ../lib-func/code_datasize.c -o l1vm -lpthread -Os -fomit-frame-pointer -funit-at-a-time -s -Wl,--export-all-symbols -mwindows -mconsole
+clang -Wall -Wextra main.c load-object.c ../lib-func/string.c ../lib-func/code_datasize.c ../lib-func/memory_bounds.c -o l1vm -lpthread -Os -fomit-frame-pointer -funit-at-a-time -s -Wl,--export-all-symbols -mwindows -mconsole
