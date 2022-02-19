8df035632cc7a49489c723e1324a4ce473d9bbc8
Stefan Pietzonke <spietzonke@gmail.com>
2022-02-19 15:19:56 +0100

    Update: egypt callgraph scripts

--- assemb/egypt-callgraph.sh
+++ assemb/egypt-callgraph.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm *.o
-gcc -Wall main.c ../lib-func/file.c checkd.c ../lib-func/string.c -fdump-rtl-expand -o l1asm -g;
+gcc -Wall main.c ../lib-func/file.c checkd.c ../lib-func/string.c ../lib-func/code_datasize.c -fdump-rtl-expand -o l1asm -g;
 
 egypt *.expand | dot -Grankdir=LR -Tps -o callgraph.ps
 rm *.o
--- vm/egypt-callgraph.sh
+++ vm/egypt-callgraph.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm *.o
-gcc -Wall main.c load-object.c ../lib-func/string.c -o l1vm-nojit -lm -ldl -lpthread -lstdc++ -lSDL2 -lSDL2_gfx -lSDL2_image -lSDL2_ttf -Os -g -fdump-rtl-expand -fomit-frame-pointer -Wl,--export-dynamic
+gcc -Wall main.c load-object.c ../lib-func/string.c ../lib-func/code_datasize.c ../lib-func/memory_bounds.c -o l1vm-nojit -lm -ldl -lpthread -lstdc++ -lSDL2 -lSDL2_gfx -lSDL2_image -lSDL2_ttf -Os -g -fdump-rtl-expand -fomit-frame-pointer -Wl,--export-dynamic
 
 egypt *.expand | dot -Grankdir=LR -Tps -o callgraph.ps
 rm *.o
