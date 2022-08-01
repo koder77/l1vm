d7fed6c647fbd1dcdf675c40237954d4bd554b4b
Stefan <spietzonke@gmail.com>
2022-08-01 15:39:05 +0200

    Clang GitHub clone build script added

--- clang-toolchain/build-toolchain-git.sh
+++ clang-toolchain/build-toolchain-git.sh
@@ -2,7 +2,7 @@
 echo "L1VM - build clang git toolchain"
 #!get files from llvm.org
 echo "downloading archives from llvm.org github..."
-#git clone https://github.com/llvm/llvm-project.git
+git clone https://github.com/llvm/llvm-project.git
 cd llvm-project
 cmake -S llvm -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/l1vm-clang-git ..
 cmake --build build -j 4
