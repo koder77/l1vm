3a38378046852f9c76bff3cf5ad76e1693e66acf
Stefan <spietzonke@gmail.com>
2022-12-31 01:30:23 +0100

    added lib/train-fann.l1com - new FANN train and save functions

--- vm/modules/fann/fann-lib.c
+++ vm/modules/fann/fann-lib.c
@@ -109,6 +109,85 @@ S8 get_free_fann (void)
 	return (-1);
 }
 
+U1 *fann_create_ann (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    S8 handle ALIGN;
+	S8 layers ALIGN;
+    S8 inputs ALIGN;
+    S8 outputs ALIGN;
+    S8 neurons_hidden ALIGN;
+    S8 ret = 1;
+
+    sp = stpopi ((U1 *) &outputs, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_create_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+    sp = stpopi ((U1 *) &neurons_hidden, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_create_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+    sp = stpopi ((U1 *) &inputs, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_create_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+    sp = stpopi ((U1 *) &layers, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_create_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+	handle = get_free_fann ();
+	if (handle == -1)
+	{
+		// ERROR no more free handle!
+
+		printf ("fann_create_ann: no more fann slot free!\n");
+		return (NULL);
+	}
+
+	if (handle < 0 || handle >= fannmax)
+    {
+        printf ("ERROR: fann_create_ann: handle out of range!\n");
+        return (NULL);
+    }
+
+    fanns[handle].ann = (struct fann *) fann_create_standard (layers, inputs, neurons_hidden, outputs);
+    if (fanns[handle].ann == NULL)
+    {
+        printf ("fann_create_ann: can't allocate fann!\n");
+        return (NULL);
+    }
+
+    fann_set_activation_function_hidden(fanns[handle].ann, FANN_SIGMOID_SYMMETRIC);
+    fann_set_activation_function_output(fanns[handle].ann, FANN_SIGMOID_SYMMETRIC);
+
+    fanns[handle].state = ANNOPEN;
+
+    // return handle
+	sp = stpushi (handle, sp, sp_bottom);
+	if (sp == NULL)
+	{
+		// error
+		printf ("fann_create_ann: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+	return (sp);
+}
+
 U1 *fann_read_ann (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
 {
 	S8 nameaddr ALIGN;
@@ -163,6 +242,59 @@ U1 *fann_read_ann (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
 	{
 		// error
 		printf ("fann_read_ann: ERROR: stack corrupt!\n");
+		return (NULL);
+	}
+	return (sp);
+}
+
+U1 *fann_save_ann (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+	S8 nameaddr ALIGN;
+	S8 handle ALIGN;
+
+	U1 sandbox_filename[256];
+    S8 ret ALIGN = 1;
+
+    sp = stpopi ((U1 *) &nameaddr, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_save_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+    sp = stpopi ((U1 *) &handle, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_save_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+	if (handle < 0 || handle >= fannmax)
+    {
+        printf ("ERROR: fann_save_ann: handle out of range!\n");
+        return (NULL);
+    }
+
+#if SANDBOX
+	if (get_sandbox_filename (&data[nameaddr], sandbox_filename, 255) != 0)
+	{
+		printf ("ERROR: fann_save_ann: ERROR filename illegal: %s\n", &data[nameaddr]);
+		return (NULL);
+	}
+
+    ret = fann_save (fanns[handle].ann, (const char *) sandbox_filename);
+#else
+    ret = fann_save (fanns[handle].ann, (const char *) &data[nameaddr]);
+#endif
+
+	// return error code
+	sp = stpushi (ret, sp, sp_bottom);
+	if (sp == NULL)
+	{
+		// error
+		printf ("fann_save_ann: ERROR: stack corrupt!\n");
 		return (NULL);
 	}
 	return (sp);
@@ -305,4 +437,126 @@ U1 *fann_free_ann (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
 	fann_destroy (fanns[handle].ann);
 	fanns[handle].state = ANNCLOSED;
 	return (sp);
+}
+
+U1 *fann_train_ann (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+	S8 inputs ALIGN;
+	S8 outputs ALIGN;
+	S8 inputs_num ALIGN;
+	S8 outputs_num ALIGN;
+	S8 handle ALIGN;
+
+	S8 i ALIGN;
+	S8 j ALIGN;
+	S8 offset ALIGN;
+
+	U1 *src_p;
+	U1 *dst_p;
+	F8 data_f ALIGN;
+
+	fann_type *input_f = NULL;
+    fann_type *output_f = NULL;
+
+    sp = stpopi ((U1 *) &outputs_num, sp, sp_top);
+	if (sp == NULL)
+	{
+	   // error
+	   printf ("ERROR: fann_train_ann: ERROR: stack corrupt!\n");
+	   return (NULL);
+	}
+
+	sp = stpopi ((U1 *) &inputs_num, sp, sp_top);
+	if (sp == NULL)
+	{
+	   // error
+	   printf ("ERROR: fann_train_ann: ERROR: stack corrupt!\n");
+	   return (NULL);
+	}
+
+	sp = stpopi ((U1 *) &outputs, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_train_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+	sp = stpopi ((U1 *) &inputs, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_train_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+	sp = stpopi ((U1 *) &handle, sp, sp_top);
+    if (sp == NULL)
+    {
+ 	   // error
+ 	   printf ("ERROR: fann_train_ann: ERROR: stack corrupt!\n");
+ 	   return (NULL);
+    }
+
+	if (handle < 0 || handle >= fannmax)
+    {
+        printf ("ERROR: fann_train_ann: handle out of range!\n");
+        return (NULL);
+    }
+
+/*
+    if (fanns[handle].state != ANNOPEN)
+	{
+		printf ("ERROR: fann_train_ann: handle not loaded!\n");
+        return (NULL);
+    }
+*/
+
+	input_f = calloc (inputs_num, sizeof (F8));
+	if (input_f == NULL)
+	{
+		printf ("ERROR: fann_train_ann: out of memory!\n");
+		return (NULL);
+	}
+
+    output_f = calloc (outputs_num, sizeof (F8));
+	if (input_f == NULL)
+	{
+		printf ("ERROR: fann_train_ann: out of memory!\n");
+		return (NULL);
+	}
+
+	// It's so ugly, but works!!!
+
+	offset = 0;
+	for (j = 0; j < inputs_num; j++)
+	{
+		src_p = &data[inputs + offset];
+		dst_p = (U1 *) &data_f;
+		for (i = 0; i < sizeof (F8); i++)
+		{
+			*dst_p++ = *src_p++;
+		}
+		input_f[j] = data_f;
+		offset += sizeof (F8);
+	}
+
+    offset = 0;
+	for (j = 0; j < outputs_num; j++)
+	{
+		src_p = &data[outputs + offset];
+		dst_p = (U1 *) &data_f;
+		for (i = 0; i < sizeof (F8); i++)
+		{
+			*dst_p++ = *src_p++;
+		}
+		output_f[j] = data_f;
+		offset += sizeof (F8);
+	}
+
+    fann_train (fanns[handle].ann, input_f, output_f);
+
+    free (output_f);
+	free (input_f);
+    return (sp);
 }
