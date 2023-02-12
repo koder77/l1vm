90eff0d906228761b556e72cedfa1df6434d73d4
Stefan <spietzonke@gmail.com>
2023-02-12 18:57:28 +0100

    build script update

--- build-all.sh
+++ build-all.sh
@@ -14,7 +14,7 @@ done
 for i in prog/*.l1com
 do
 	filename=$(basename "$i" .l1com)
-	./build.sh prog/$filename -sizes 1000000 1000000000
+	./l1vm-build.sh prog/$filename -sizes 1000000 1000000000
 done
 rm prog/*.l1asm.l1dbg
 exit 0
