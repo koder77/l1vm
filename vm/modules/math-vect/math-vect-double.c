92fbf5636b759d0cc2e5dd679006e185bfc6edf4
koder77 <spietzonke@gmail.com>
2023-08-23 11:59:12 +0200

    JIT compiler lib update

--- vm/modules/math-vect/math-vect-double.c
+++ vm/modules/math-vect/math-vect-double.c
@@ -184,9 +184,6 @@ int compare_double_inc (const void* a, const void* b)
     if (arg1 < arg2) return -1;
     if (arg1 > arg2) return 1;
     return 0;
-
-    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
-    // return arg1 - arg2; // erroneous shortcut (fails if INT_MIN is present)
 }
 
 int compare_double_dec (const void* a, const void* b)
@@ -197,9 +194,6 @@ int compare_double_dec (const void* a, const void* b)
     if (arg1 > arg2) return -1;
     if (arg1 < arg2) return 1;
     return 0;
-
-    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
-    // return arg1 - arg2; // erroneous shortcut (fails if INT_MIN is present)
 }
 
 U1 *mvect_sort_double_inc (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
--- vm/modules/math-vect/math-vect-int.c
+++ vm/modules/math-vect/math-vect-int.c
@@ -190,9 +190,6 @@ int compare_int_inc (const void* a, const void* b)
     if (arg1 < arg2) return -1;
     if (arg1 > arg2) return 1;
     return 0;
-
-    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
-    // return arg1 - arg2; // erroneous shortcut (fails if INT_MIN is present)
 }
 
 int compare_int_dec (const void* a, const void* b)
@@ -203,9 +200,6 @@ int compare_int_dec (const void* a, const void* b)
     if (arg1 > arg2) return -1;
     if (arg1 < arg2) return 1;
     return 0;
-
-    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
-    // return arg1 - arg2; // erroneous shortcut (fails if INT_MIN is present)
 }
 
 U1 *mvect_sort_int_inc (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
