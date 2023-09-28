5e9cba86589f14e5be0ba2f070847eb2972f1609
koder77 <spietzonke@gmail.com>
2023-09-28 15:41:30 +0200

    Update: Clang toolchain git build

--- clang-toolchain/build-toolchain-git.sh
+++ clang-toolchain/build-toolchain-git.sh
@@ -3,7 +3,7 @@ echo "L1VM - build Clang git toolchain"
 echo "Cloning latest Clang from GitHub..."
 git clone https://github.com/llvm/llvm-project.git
 cd llvm-project
-cmake -S llvm -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/l1vm-clang-git ..
+cmake -S llvm -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;lld" -DCMAKE_INSTALL_PREFIX=~/l1vm-clang-git ..
 cmake --build build -j 4
 cd build
 make install
