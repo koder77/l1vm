e9af91ac1084feed94eb2947d5e2800b1975e8e8
koder77 <stefan@localhost.localdomain>
2023-10-29 14:16:21 +0100

    websever local update

--- vm/make-cli.sh
+++ vm/make-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set vm/main.c JIT_COMPILER to 0 and compile using this script
-if clang -Wall -Wextra main.c load-object.c ../lib-func/string.c ../lib-func/code_datasize.c ../lib-func/memory_bounds.c -o l1vm-cli -lm -ldl -lpthread -O2 -g -march=native -fomit-frame-pointer -Wl,--export-dynamic; then
+if $CC -Wall -Wextra main.c load-object.c ../lib-func/string.c ../lib-func/code_datasize.c ../lib-func/memory_bounds.c -o l1vm-cli -lm -ldl -lpthread -O2 -g -march=native -fomit-frame-pointer -Wl,--export-dynamic; then
 	exit 0
 else
 	exit 1
