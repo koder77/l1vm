02c4eb55fff5ff6783c989163ffb695ba9bdb556
koder77 <spietzonke@gmail.com>
2024-03-16 20:13:45 +0100

    JIT-compiler update

--- libjit/make-win.sh
+++ libjit/make-win.sh
@@ -3,4 +3,4 @@ rm *.o
 rm *.cpo
 rm *.so
 clang++ -I../include  -I/usr/local/include -O2 -fomit-frame-pointer -Wall -c -o jit.cpo jit.cpp
-clang -o libl1vm-jit.so jit.cpo  -L/usr/local/lib -shared -lasmjit -lstdc++
+clang -o libl1vm-jit.so jit.cpo  -L/usr/local/lib -shared -lasmjit-l1vm -lstdc++
