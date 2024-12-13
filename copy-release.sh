4b0758f150d826cfe54dc4c1a293abbf7b580671
koder77 <spietzonke@gmail.com>
2024-12-13 23:12:41 +0100

    copy-release.sh update

--- copy-release.sh
+++ copy-release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir bin-release
 cp modules/*.so bin-release
-cp assemb/l1asm bin-release
-cp comp/l1com bin-release
-cp disasm/disasm bin-release
-cp prepro/l1pre bin-release
-cp linter/l1vm-linter bin-release
-cp vm/l1vm bin-release
-cp vm/l1vm-nojit bin-release
-cp zerobuild/zerobuild bin-release
+cp assemb/l1asm* bin-release
+cp comp/l1com* bin-release
+cp disasm/l1vm-disasm* bin-release
+cp prepro/l1pre* bin-release
+cp linter/l1vm-linter* bin-release
+cp vm/l1vm* bin-release
+cp vm/l1vm-nojit* bin-release
+cp zerobuild/zerobuild*  bin-release
