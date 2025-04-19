03121ca0fd50d2484fe3fdf3c435eb7780e29e73
koder77 <info@midnight-coding.de>
2025-04-19 22:12:51 +0200

    fractalix programs cleanup

--- vm/main.c
+++ vm/main.c
@@ -1,25 +1,3 @@
-/*
- * This file main.c is part of L1vm.
- *
- * (c) Copyright Stefan Pietzonke (info@midnight-coding.de), 2017
- *
- * L1vm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * L1vm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-//  l1vm RISC VM
-//
-//
 
 #include "jit.h"
 #include "../include/global.h"
