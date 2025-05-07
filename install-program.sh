383ef54fc45401f5031ee027ab65369ea870500a
koder77 <info@midnight-coding.de>
2025-05-07 20:15:36 +0200

    Program install script update

--- install-program.sh
+++ install-program.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+if [ $# -eq 0 ]; then
+    >&2 echo "No arguments provided! Need full l1vm program archive name!"
+    exit 1
+fi
+
 tar xjf $1 -C ~/l1vm/
+echo "Package installed!"
+exit 0
