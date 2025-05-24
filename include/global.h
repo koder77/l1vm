74036705e9e81e62f76890eedce7bd36774a460b
koder77 <info@midnight-coding.de>
2025-05-24 10:25:26 +0200

    global.h: version number update

--- include/global.h
+++ include/global.h
@@ -204,7 +204,7 @@
 
 // info strings:
 #define COPYRIGHT_STR " 2025 (C) 2017-2025 Stefan Pietzonke - software research"
-#define VM_VERSION_STR "3.4.2"
+#define VM_VERSION_STR "3.4.3"
 #define MOTTO_STR "Level 1 VM"
 
 // no user defined definitions below this section! ============================
