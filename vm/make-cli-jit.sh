23d809e7c24a305e4e294f5f6bbbedd8d5e38170
koder77 <info@midnight-coding.de>
2025-06-15 16:39:52 +0200

    VM build script update

--- vm/make-cli-jit.sh
+++ vm/make-cli-jit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set vm/main.c JIT_COMPILER to 0 and compile using this script
-if clang -Wall main.c load-object.c debugger.c ../lib-func/string.c ../lib-func/code_datasize.c ../lib-func/memory_bounds.c -o l1vm-cli -lm -ldl -lasmjit -ll1vm-jit -lpthread -O2 -g -march=native -fomit-frame-pointer -Wl,--export-dynamic; then
+if clang -Wall main.c load-object.c debugger.c ../lib-func/string.c ../lib-func/code_datasize.c ../lib-func/memory_bounds.c -o l1vm-cli -lm -ldl -L../asmjit/build -lasmjit-l1vm -lpthread -O2 -g -march=native -fomit-frame-pointer -Wl,--export-dynamic; then
 	exit 0
 else
 	exit 1
