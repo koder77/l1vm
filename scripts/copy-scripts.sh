a50ba9c802e68dfe419b43ade733005733e8d645
koder77 <info@midnight-coding.de>
2025-08-21 04:39:56 +0200

    scripts/copy-scripts update

--- scripts/copy-scripts.sh
+++ scripts/copy-scripts.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-cp *.sh ~/l1vm/bin/
+cp install-program.sh ~/l1vm/bin/
+cp l1vm-build.sh ~/l1vm/bin/
+cp l1vm-build-debug.sh ~/l1vm/bin/
+cp l1vm-build-lint.sh ~/l1vm/bin/
+cp l1vm-build-lint-win.sh ~/l1vm/bin/
+cp l1vm-build-lint-win-wspace.sh ~/l1vm/bin/
+cp l1vm-build-lint-wspace.sh ~/l1vm/bin/
+cp l1vm-build-win.sh ~/l1vm/bin/
+cp l1vm-build-win-wspace.sh ~/l1vm/bin/
+cp l1vm-build-wspace.sh ~/l1vm/bin/
+cp l1vm-debug.sh ~/l1vm/bin/
+cp new-program.sh ~/l1vm/bin/
+cp set-path.sh ~/l1vm/bin
+cp statify.sh ~/l1vm/bin
