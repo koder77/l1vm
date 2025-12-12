3dff35a230c1bbfc3b24e6e4cd0d32300f281490
koder77 <info@midnight-coding.de>
2025-12-12 08:53:40 +0100

    VM: GPS module added

---
+++ vm/modules/gps/gps.c
@@ -0,0 +1,282 @@
+/*
+ * This file gps.c is part of L1vm.
+ *
+ * (c) Copyright Stefan Pietzonke (info@midnight-coding.de), 2025
+ *
+ * L1vm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * L1vm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// GPSD GPS receiver module
+
+#include "../../../include/global.h"
+#include "../../../include/stack.h"
+
+#include <gps.h>
+
+#define SERVER_NAME "localhost"
+#define SERVER_PORT "2947"
+#define MS_TO_KMH 3.6
+
+struct gps_data_t g_gpsdata;
+int ret;
+int gps_init = 0;
+
+// protos
+S2 memory_bounds (S8 start, S8 offset_access);
+
+struct data_info data_info[MAXDATAINFO];
+S8 data_info_ind;
+
+S2 init_memory_bounds (struct data_info *data_info_orig, S8 data_info_ind_orig)
+{
+    memcpy (&data_info, &data_info_orig, sizeof (data_info_orig));
+    data_info_ind = data_info_ind_orig;
+
+    return (0);
+}
+
+U1 *gps_setup (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    ret = gps_open (SERVER_NAME, SERVER_PORT, &g_gpsdata);
+    if (ret != 0)
+    {
+        //printf("[GPS] Can't open...bye!\r\n");
+        sp = stpushi (1, sp, sp_bottom);
+        if (sp == NULL)
+        {
+            // error
+            printf ("gps_setup: ERROR: stack corrupt!\n");
+            return (NULL);
+        }
+    }
+    else
+    {
+        // GPS open
+        gps_init = 1;
+        gps_stream (&g_gpsdata, WATCH_ENABLE | WATCH_JSON | WATCH_TIMING, NULL);
+
+        sp = stpushi (0, sp, sp_bottom);
+        if (sp == NULL)
+        {
+            // error
+            printf ("gps_setup: ERROR: stack corrupt!\n");
+            return (NULL);
+        }
+    }
+    return (sp);
+}
+
+U1 *gps_quit (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    if (gps_init == 1)
+    {
+        gps_stream(&g_gpsdata, WATCH_DISABLE, NULL);
+        gps_close(&g_gpsdata);
+
+        gps_init = 0; // closed
+    }
+
+    return (sp);
+}
+
+U1 *gps_read_data (U1 *sp, U1 *sp_top, U1 *sp_bottom, U1 *data)
+{
+    double latitude = 0.0, longitude = 0.0, altitude = 0.0, speed_kmh = 0.0, track = 0.0;
+    S8 satellites_used = 0;
+    S8 timestrptr;
+    S2 result = 0;
+
+    sp = stpopi ((U1 *) &timestrptr, sp, sp_top);
+    if (sp == NULL)
+    {
+        printf ("gps_read_data: ERROR: stack corrupt on input!\n");
+        return (NULL);
+    }
+
+    if (memory_bounds (timestrptr, 30) != 0)
+    {
+        printf ("gps_read_data: ERROR: String pointer out of bounds!\n");
+        return (NULL); // FATAL error time string overflow
+    }
+
+    if (gps_waiting (&g_gpsdata, 50000))
+    {
+        if (-1 == gps_read (&g_gpsdata, NULL, 0))
+        {
+            // return empty data
+            sp = stpushi (satellites_used, sp, sp_bottom);
+            if (sp == NULL)
+            {
+                // error
+                printf ("gps_read_data: ERROR: stack corrupt!\n");
+                return (NULL);
+            }
+
+            sp = stpushd (speed_kmh, sp, sp_bottom);
+            if (sp == NULL)
+            {
+                // error
+                printf ("gps_read_data: ERROR: stack corrupt!\n");
+                return (NULL);
+            }
+
+            sp = stpushd (track, sp, sp_bottom);
+            if (sp == NULL)
+            {
+                // error
+                printf ("gps_read_data: ERROR: stack corrupt!\n");
+                return (NULL);
+            }
+
+            sp = stpushd (altitude, sp, sp_bottom);
+            if (sp == NULL)
+            {
+                // error
+                printf ("gps_read_data: ERROR: stack corrupt!\n");
+                return (NULL);
+            }
+
+            sp = stpushd (longitude, sp, sp_bottom);
+            if (sp == NULL)
+            {
+                // error
+                printf ("gps_read_data: ERROR: stack corrupt!\n");
+                return (NULL);
+            }
+
+            sp = stpushd (latitude, sp, sp_bottom);
+            if (sp == NULL)
+            {
+                // error
+                printf ("gps_read_data: ERROR: stack corrupt!\n");
+                return (NULL);
+            }
+
+            // error code no GPS read possible!
+            sp = stpushi (5, sp, sp_bottom);
+            if (sp == NULL)
+            {
+                // error
+                printf ("gps_read_data: ERROR: stack corrupt!\n");
+                return (NULL);
+            }
+
+            data[timestrptr] = '\0';   // empty time string
+
+            return (sp);    // exit
+        }
+        else
+        {
+            if (g_gpsdata.fix.mode >= MODE_2D)
+            {
+                if (isfinite(g_gpsdata.fix.latitude) && isfinite(g_gpsdata.fix.longitude) && isfinite(g_gpsdata.fix.altitude) && isfinite(g_gpsdata.fix.speed) && isfinite(g_gpsdata.fix.track))
+                {
+                    latitude = g_gpsdata.fix.latitude;
+                    longitude = g_gpsdata.fix.longitude;
+                    altitude = g_gpsdata.fix.altitude;
+
+                    speed_kmh = g_gpsdata.fix.speed * MS_TO_KMH;
+                    track = g_gpsdata.fix.track;
+                    satellites_used = g_gpsdata.satellites_used;
+
+                    time_t rawtime = g_gpsdata.fix.time.tv_sec;
+                    char time_buffer[30];
+
+                    strncpy(time_buffer, ctime(&rawtime), sizeof(time_buffer) - 1);
+                    if (time_buffer[strlen(time_buffer) - 1] == '\n')
+                    {
+                        time_buffer[strlen(time_buffer) - 1] = '\0';
+                    }
+
+                    memcpy((U1*) data + timestrptr, time_buffer, strlen(time_buffer) + 1);
+
+                    result = 0; // success
+                }
+                else
+                {
+                    result = 3; // Fix, but no valid data (NaN)
+                }
+            }
+            else
+            {
+                result = 1; // No 2D/3D Fix
+            }
+        }
+    }
+    else
+    {
+        usleep(10000);  // short break
+        result = 4; // No new data
+    }
+
+    // return data
+    sp = stpushi (satellites_used, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("gps_read_data: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    sp = stpushd (speed_kmh, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("gps_read_data: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    sp = stpushd (track, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("gps_read_data: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    sp = stpushd (altitude, sp, sp_bottom);
+    if (sp == NULL)
+    {
+         // error
+        printf ("gps_read_data: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    sp = stpushd (longitude, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("gps_read_data: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    sp = stpushd (latitude, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("gps_read_data: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    // error code
+    sp = stpushi (result, sp, sp_bottom);
+    if (sp == NULL)
+    {
+        // error
+        printf ("gps_read_data: ERROR: stack corrupt!\n");
+        return (NULL);
+    }
+
+    return (sp);
+}
---
+++ vm/modules/gps/make.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+clang-19 gps-read.c -o gps-read -lgps
