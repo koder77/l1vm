c38832283d641a9d0ede0ea1e5f5d01239c0f652
koder77 <info@midnight-coding.de>
2026-04-25 11:34:09 +0200

    scripts build programs update

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
-# build demo programs
+# build demo programs (recursive & path-safe)
 
-cd ~/l1vm
+# Immer vom Basisverzeichnis ausgehen
+BASE_DIR="$HOME/l1vm"
+cd "$BASE_DIR" || exit 1
 
-for i in prog/*.l1asm
-do
-	filename=$(basename "$i" .l1asm)
-	l1asm prog/$filename
+echo "Starting build in $BASE_DIR..."
+
+# 1. Alle .l1asm Dateien finden
+# Wir suchen im Verzeichnis "prog" relativ zum BASE_DIR
+find prog -type f -name "*.l1asm" | while read -r i; do
+    # Pfad ohne Endung (z.B. prog/linter/test)
+    filepath="${i%.l1asm}"
+    echo "Assembling: $i"
+    l1asm "$filepath"
 done
-	
-for i in prog/*.l1com
-do
-	filename=$(basename "$i" .l1com)
-	l1vm-build.sh prog/$filename -sizes 1000000 1000000000
+
+# 2. Alle .l1com Dateien finden
+find prog -type f -name "*.l1com" | while read -r i; do
+    filepath="${i%.l1com}"
+    echo "Building: $i"
+    # Aufruf des Build-Scripts mit dem Pfad relativ zu ~/l1vm
+    l1vm-build.sh "$filepath" -sizes 1000000 1000000000
 done
-rm prog/*.l1asm.l1dbg
-exit 0
+
+# 3. Debug-Dateien aufräumen
+find prog -type f -name "*.l1asm.l1dbg" -delete
+find prog -type f -name "*.l1dbg" -delete
+
+echo "Build finished."
