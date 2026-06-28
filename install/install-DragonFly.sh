9b5ac21c04bc351dbee25bc22b56ef7dac28b1e4
koder77 <info@midnight-coding.de>
2026-06-28 16:08:06 +0200

    DragonFly BSD install base pkg checksum

--- install/install-DragonFly.sh
+++ install/install-DragonFly.sh
@@ -3,7 +3,7 @@
 
 echo "Install base package with includes and programs..."
 # Known-good SHA256 checksum for l1vm-base-pkg.tar.bz2 - update when the package is updated
-EXPECTED_SHA256="PLACEHOLDER_REPLACE_WITH_KNOWN_SHA256_HASH"
+EXPECTED_SHA256="b0bc1a8d684473f6b013b8a31aa25d46ee5c5c78db08a7bbd164e1357621ced4"
 ../scripts/download-pkg.sh https://midnight-coding.de/blog/assets/l1vm/l1vm-base-pkg.tar.bz2 l1vm-base-pkg.tar.bz2
 ACTUAL_SHA256=$(sha256 -q l1vm-base-pkg.tar.bz2 2>/dev/null || openssl dgst -sha256 l1vm-base-pkg.tar.bz2 2>/dev/null | awk '{print $NF}')
 if [ "$ACTUAL_SHA256" != "$EXPECTED_SHA256" ]; then
