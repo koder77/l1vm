a6f02e748c36f16518e512232a3d22d19aa833f8
koder77 <info@midnight-coding.de>
2026-07-04 15:36:41 +0200

    brackets-code: added GPL license

--- brackets-code/brackets-code.h
+++ brackets-code/brackets-code.h
@@ -1,3 +1,22 @@
+/*
+ * This file brackets-code.h is part of L1vm.
+ *
+ * (c) Copyright Stefan Pietzonke (info@midnight-coding.de), 2026
+ *
+ * L1vm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * L1vm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef BRACKETS_CODE_H
 #define BRACKETS_CODE_H
 
