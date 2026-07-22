71c07a1103cf560e2d2d63e546a0d11b864e1472
koder77 <info@midnight-coding.de>
2026-07-22 16:44:48 +0200

    brackets-code removed to separate repo

--- brackets-code/l1vm-example-code/README.txt
+++
@@ -1,2 +0,0 @@
-Put the l1vm directories here: "prog", "lib" and "include".
-The content here will be searched by brackets-code!
--- brackets-code/run-infer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-# get infer linter from: https://fbinfer.com/
-
-infer run -- clang -c brackets-code.c dsl.c embed.c learn.c
