9c8b043f8c059425e745c2e16935229946c787c3
koder77 <info@midnight-coding.de>
2026-09-10 21:10:00 +0200

    brackets-llm system-prompt

--- brackets-llm/run.sh
+++ brackets-llm/run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 export BRACKETS_LLM_MODEL=Qwen3.6-35B-A3B-UD-Q4_K_XL.gguf && build/brackets-llm
+#export BRACKETS_LLM_MODEL=Qwen3.8-27B-UD-IQ2_XXS.gguf && build/brackets-llm
+#export BRACKETS_LLM_MODEL=gemma-4-26B-A4B-it-UD-Q4_K_M.gguf && build/brackets-llm
